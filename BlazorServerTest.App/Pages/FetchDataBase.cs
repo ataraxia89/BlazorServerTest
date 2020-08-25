b2c522367f6d53a0af0f784085791b87b7d57d84
John Foster <john.foster@martial.tech>
2020-08-25 22:33:05 +0100

    Added test solution

---
+++ BlazorServerTest.App/Pages/FetchDataBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlazorServerTest.App.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerTest.App.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        [BindProperty]
+        public WeatherForecast[] forecasts { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
